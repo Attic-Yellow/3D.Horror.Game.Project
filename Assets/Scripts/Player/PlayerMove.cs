acd6904ee04bce4b308edd055877fda6e9a9c84c
Attic_Yellow <chtori1202@gmail.com>
2024-02-26 21:31:20 +0900

    스크립트 수정 :
    - 플레이어 무브 스크립트 수정

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -8,8 +8,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     private enum State
     {
-       Standing,
-       Crouch
+        Standing,
+        Crouch
     }
     private State state;
     private CharacterController controller;
@@ -17,10 +17,10 @@ public class PlayerMove : MonoBehaviour
 
     public float moveSpeed;
     public float sprintSpeed;
+    public float crouchWalkSpeed;
+    public float crouchRunSpeed;
+    public float gravityScale = 1f;
 
-
-    
-    float moveDirY;
 
     private float currentSpeed;
     private bool isRun = false;
@@ -28,19 +28,20 @@ public class PlayerMove : MonoBehaviour
     private Vector2 inputValue;
     private Vector3 moveDir;
     public bool isMoving;
-  
-    
+
+
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         state = State.Standing;
-  
+        animator.SetLayerWeight(1, 0);
     }
 
     private void Update()
     {
+        Ani();
         if (GameManager.instance.overlayManager.CheckOnOverlay())
         {
             return;
@@ -49,24 +50,30 @@ public class PlayerMove : MonoBehaviour
         isMoving = inputValue.magnitude > 0f;
         switch (state)
         {
-          case State.Standing:
+            case State.Standing:
+
+                if (inputValue.y < 0) //뒤로 가는키를 누르면 이속감소
+                {
+                    currentSpeed = isRun ? sprintSpeed * 0.5f : moveSpeed * 0.7f;
+                }
+                else
+                {
+                    currentSpeed = isRun ? sprintSpeed : moveSpeed;
+                }
 
                 break;
-          case State.Crouch:
+            case State.Crouch:
+
+                if (inputValue.y < 0)
+                {
+                    currentSpeed = isRun ? crouchRunSpeed * 0.5f : crouchWalkSpeed * 0.7f;
+                }
+                else
+                {
+                    currentSpeed = isRun ? crouchRunSpeed : crouchWalkSpeed;
+                }
 
                 break;
-        }
-     
-
-      
-
-        if (inputValue.y < 0) //뒤로 가는키를 누르면 이속감소
-        {
-            currentSpeed = isRun ? sprintSpeed * 0.5f : moveSpeed * 0.7f;
-        }
-        else
-        {
-            currentSpeed = isRun ? sprintSpeed : moveSpeed;
         }
 
         // 카메라 방향에 기반한 플레이어의 이동 방향 계산
@@ -78,30 +85,38 @@ public class PlayerMove : MonoBehaviour
         right.Normalize();
 
         moveDir = (forward * inputValue.y + right * inputValue.x) * currentSpeed;
-        moveDir.y = 0;
-
+        Gravity();
         controller.Move(moveDir * Time.deltaTime);
-
 
     }
 
-
+    private void Gravity()
+    {
+        if (!controller.isGrounded)
+        {
+            moveDir.y -= gravityScale;
+        }
+        else
+        {
+            moveDir.y = 0f;
+        }
+    }
 
     public void OnMove(InputValue _value)
     {
         inputValue = _value.Get<Vector2>();
     }
 
-   public void OnRun()
+    public void OnRun()
     {
         isRun = true;
-     
+
     }
 
     public void OnRunCancle()
     {
         isRun = false;
-      
+
     }
     public void OnCrouch()
     {
@@ -109,9 +124,22 @@ public class PlayerMove : MonoBehaviour
     }
     private void Ani()
     {
-        animator.SetFloat("Dir", moveDir.magnitude * Time.deltaTime);
-        // animator.SetFloat("")
-    }
+        animator.SetFloat("Velocity", new Vector3(moveDir.x, 0, moveDir.z).magnitude);
+        animator.SetFloat("XDir", inputValue.x * currentSpeed);
+        animator.SetFloat("ZDir", inputValue.y * currentSpeed);
 
-  
+        animator.SetBool("IsCrouch", isCrouch);
+        animator.SetBool("IsRun", isRun);
+
+    }
+    public void TakeOutAni()
+    {
+        animator.SetLayerWeight(1, 1f); //애니메이터의 두번쨰 레이어의 weight를 1로
+        animator.SetTrigger("TakeOut");
+    }
+    public void WeightZero() // 올리는 애니메이션 끝날때
+    {
+        animator.SetLayerWeight(1, 0f);
+        //TODO : 펼쳐지는 애니메이션 및 카메라 무브.
+    }
 }
