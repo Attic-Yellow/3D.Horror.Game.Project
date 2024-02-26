e88a24cd6dc4df1fcc64532c7731c0ac45f615dd
JSChoi99 <139032888+JSChoi99@users.noreply.github.com>
2024-02-26 21:34:16 +0900

    pull check

--- Assets/Scripts/Scene/GameScene/Missions/FireExit/FireExit.cs
+++ Assets/Scripts/Scene/GameScene/Missions/FireExit/FireExit.cs
@@ -10,7 +10,7 @@ public class FireExit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("¤·¤·");
     }
 
     // Update is called once per frame
